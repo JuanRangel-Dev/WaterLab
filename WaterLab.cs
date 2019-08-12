1ae2b85100ac315adef3085a3d95d702d69b5ed4
Juan <52424011+rangel-juan@users.noreply.github.com>
2019-08-12 09:33:08 -0500

    Add files via upload

---
+++ WaterLab.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Wednesday
+{
+    class WaterLab
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Write a program that will:
+             * 
+             *              -Assume that a gallon of water weighs 8.33 pounds
+             *              -Prompt the user to enter a number of gallons
+             *              -Display the total weight of the water in pounds
+             *              
+             */
+
+
+
+            Console.Write("Please enter your amount of Gallons!: ");
+
+            decimal dec1 = 8.33m;
+
+            Console.WriteLine(dec1 * int.Parse(Console.ReadLine()));
+
+
+        }//end Main()
+    }//end class
+}//end namespace
